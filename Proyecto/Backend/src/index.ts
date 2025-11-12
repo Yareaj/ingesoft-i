82fd7f558f990a29dafd52450c03af59425de179
jauri <jcortesca@unal.edu.co>
2025-11-12 10:55:18 -0500

    get ip local

--- Proyecto/Backend/src/index.ts
+++ Proyecto/Backend/src/index.ts
@@ -2,13 +2,16 @@ import "reflect-metadata";
 import express from "express";
 import * as dotenv from "dotenv";
 import * as path from "path";
+import getLocalIP from "./db_connection/config/getLocalIp";
 import Database from "./db_connection/db/Database";
 import { getFirstUser, registerUser } from "./db_connection/controller/UserController";
+
 
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const localIP = getLocalIP();
 
 Database.initialize()
     .then(() => {
@@ -23,7 +26,7 @@ Database.initialize()
 
         app.listen(PORT, () => {
             console.log(`🚀 Servidor Express corriendo en el puerto ${PORT}`);
-            console.log(`Endpoint de prueba: http://localhost:${PORT}/api/hello-user`);
+            console.log(`Endpoint de prueba: http://${localIP}:${PORT}/api/hello-user`);
         });
     })
     .catch((error) => console.error("❌ Error al conectar la base de datos:", error));
